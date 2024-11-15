97c5521f614add4df1b63253c287ac55d6025f20
Lilit <martirosianli14@gmail.com>
2024-11-15 23:34:43 +0400

    ToDoListApp

--- ToDoListApp/ContentView.swift
+++ ToDoListApp/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var textFieldValue: String = ""
+    //    zangvac
+    @State var tasks:[(text: String, isChecked: Bool)]=[]
+    
+    //    state nra hamar a
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack {
+            HStack{
+                TextField(
+                    "Write your task",
+                    text: $textFieldValue
+                )
+                
+                Button(action:addTask){
+                    Text("Add text")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(90)
+                }
+            }.padding(20)
+            //            List\LazyVStack
+            //            2.displaying...
+            List {
+                ForEach(tasks.indices, id: \.self) { index in
+                    HStack {
+                        Toggle(isOn: $tasks[index].isChecked) {
+                            Text(tasks[index].text)
+                                .strikethrough(tasks[index].isChecked, color: .black)
+                                .foregroundColor(tasks[index].isChecked ? .gray.opacity(0.5) : .red)
+                        }
+                        
+                        Button(action: {
+                            deleteTask(at: index)
+                        }) {
+                            Text("Delete")
+                                .padding(20)
+                                .background(Color.red)
+                                .foregroundColor(.white)
+                                .cornerRadius(120)
+                        }
+                    }
+                }
+            }
             .padding()
+        }
+    }
+    
+    func addTask() {
+        guard !textFieldValue.isEmpty else { return }
+        tasks.append((text: textFieldValue, isChecked: false))
+        textFieldValue = ""
+    }
+    
+    func deleteTask(at index: Int) {
+        tasks.remove(at: index)
     }
 }
 
@@ -19,3 +73,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
